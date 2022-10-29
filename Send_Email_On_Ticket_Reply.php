e90a6533f891478637d6114432e6ca071bc5b506
Leon <64979109+ItsLeon15@users.noreply.github.com>
2022-10-29 20:51:30 +0100

    Create Send_Email_On_Ticket_Reply.php

---
+++ Send_Email_On_Ticket_Reply.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Automatically send a email to a client when a new reply is made from a admin.
+ *
+ * @package     WHMCS
+ * @copyright   ItsLeon15
+ * @link        https://www.platinumhost.xyz
+ * @author      Leon <leon@platinumhost.xyz>
+ */
+
+use WHMCS\Database\Capsule;
+
+add_hook('TicketAdminReply', 1, function($vars) {
+
+    $ticketDetails = Capsule::table('tbltickets')
+        ->where('id', $vars['ticketid'])
+        ->first(['userid', 'tid', 'title', 'id', 'c']
+    );
+
+    $EmailData = array(
+        'id' => $ticketDetails->userid,
+        'customtype' => 'general',
+        'customsubject' => '[Ticket ID: ' . $ticketDetails->tid . '] ' . $ticketDetails->title,
+        'custommessage' => $vars['message'] . PHP_EOL . 
+        '----------------------------------------------' . PHP_EOL . 
+        'Ticket ID: ' . $ticketDetails->tid . PHP_EOL . 
+        'Subject: ' . $ticketDetails->title . PHP_EOL . 
+        'Status: ' . $vars['status'] . PHP_EOL .
+        'View Ticket: ' . '<a href="' . $vars['systemurl'] . 'viewticket.php?tid=' . $ticketDetails->tid . '&c=' . $ticketDetails->c . '">Click Here</a>' . PHP_EOL .
+        '----------------------------------------------' . PHP_EOL . 
+        PHP_EOL . 
+        'This is an automated message. Please do not reply to this email.',
+    );
+    
+    localAPI('SendEmail', $EmailData);
+});
