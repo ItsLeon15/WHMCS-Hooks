1e877ce55231d21564622c00f94d1aa029020105
Leon <64979109+ItsLeon15@users.noreply.github.com>
2023-01-17 20:25:38 +0000

    Create Credit_Balance.php

---
+++ Credit_Balance.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Adds the option to view Credit Balance in WHMCS (Client View - https://i.imgur.com/C3imJc7.png)
+ *
+ * @package     WHMCS
+ * @copyright   ItsLeon15
+ * @link        https://www.platinumhost.xyz
+ * @author      Leon <leon@platinumhost.xyz>
+ */
+
+use WHMCS\View\Menu\Item as MenuItem;
+
+add_hook('ClientAreaPrimarySidebar', 1, function (MenuItem $primarySidebar) {
+
+    $filename = basename($_SERVER['REQUEST_URI'], ".php");
+    $parseFile = explode('.', $filename);
+    $client = Menu::context("client");
+    $clientid = intval($client->id);
+    if ($parseFile['0'] !== 'clientarea' || $clientid === 0) {
+        return;
+    }
+    $primarySidebar->addChild('Client-Balance', array('label' => Lang::trans('availcreditbal'), 'uri' => '#', 'order' => '1', 'icon' => 'fa-money'));
+    $balancePanel = $primarySidebar->getChild('Client-Balance')->moveToBack()->setOrder(0);
+    $balancePanel->addChild('balance-amount', array('uri' => 'clientarea.php?action=addfunds"style="background-color: transparent !important', 'label' => '<h4 style="text-align:center; margin:0">' . formatcurrency($client->credit, $client->currencyId) . '</h4>', 'order' => 1));
+    $balancePanel->setFooterHtml('<a href="clientarea.php?action=addfunds" class="btn btn-success btn-sm btn-block"><i class="fa fa-plus"></i> ' . Lang::trans('addfunds') . '</a>');
+});
